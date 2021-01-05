5680d2490a4d6a36cc6d99e82b195962f6fb33ca
josernr25 <31230939+josernr25@users.noreply.github.com>
2021-01-04 23:02:07 -0300

    ADD-ScriptDatos

---
+++ Datos.sql
@@ -0,0 +1,17 @@
+INSERT INTO Parques(Nombre, Provincia, Created, Established, Imagen, Updated, Descripcion)
+VALUES ('Glaciar Perito Moreno', 'Santa Cruz', '2019-01-01 00:00:00.0000000', '2019-01-01 00:00:00.0000000', 'glaciar-perito-small.jpg', '2019-01-01 00:00:00.0000000', 'El glaciar Perito Moreno es una gruesa masa de hielo ubicada en el departamento Lago Argentino de la provincia de Santa Cruz, en el sudoeste de la Argentina.');
+INSERT INTO Parques(Nombre, Provincia, Created, Established, Imagen, Updated, Descripcion)
+VALUES ('Cataratas del Iguazu', 'Misiones', '2019-01-01 00:00:00.0000000', '2019-01-01 00:00:00.0000000', 'catarata-iguazu-small.jpg', '2019-01-01 00:00:00.0000000', 'ElLas Cataratas del Iguazú son un conjunto de cataratas que se localizan sobre el río Iguazú, en el límite entre la provincia de Misiones y el estado brasileño de Paraná.');
+INSERT INTO Parques(Nombre, Provincia, Created, Established, Imagen, Updated, Descripcion)
+VALUES ('Los Alerces', 'Chubut', '2019-01-01 00:00:00.0000000', '2019-01-01 00:00:00.0000000', 'alerces-small.jpg', '2019-01-01 00:00:00.0000000', 'El Parque Nacional Los Alerces es una gran área protegida y patrimonio mundial de la Unesco que se encuentra en la provincia del Chubut, Argentina.');
+INSERT INTO Parques(Nombre, Provincia, Created, Established, Imagen, Updated, Descripcion)
+VALUES ('Nahuel Huapi', 'Neuquen', '2019-01-01 00:00:00.0000000', '2019-01-01 00:00:00.0000000', 'nahuel-huapi-small.jpg', '2019-01-01 00:00:00.0000000', 'El lago Nahuel Huapi es un lago de origen glaciar de la Patagonia Argentina, es compartido por las provincias argentinas del Neuquén y de Río Negro.');
+INSERT INTO Parques(Nombre, Provincia, Created, Established, Imagen, Updated, Descripcion)
+VALUES ('Lanín', 'Neuquen', '2019-01-01 00:00:00.0000000', '2019-01-01 00:00:00.0000000', 'lanin-small.jpg', '2019-01-01 00:00:00.0000000', 'Parque nacional en el oeste de Argentina, famoso por el volcán Lanín, el lago Lácar y las araucarias.');
+INSERT INTO Parques(Nombre, Provincia, Created, Established, Imagen, Updated, Descripcion)
+VALUES ('Talampaya', 'La Rioja', '2019-01-01 00:00:00.0000000', '2019-01-01 00:00:00.0000000', 'talampaya-small.jpg', '2019-01-01 00:00:00.0000000', 'El parque nacional Talampaya se encuentra ubicado en el centro-oeste de la provincia de La Rioja en Argentina entre los departamentos de Coronel Felipe Varela e Independencia.');
+
+INSERT INTO Atracciones(Nombre, Imagen, Imagen2, Imagen3, Descripcion, ParqueId, Created, Updated)
+VALUES ('Garganta del Diablo', 'garganta-diablo.jpg', null, null, 'Imponente cascada en un parque nacional.', 9, '0001-01-01 00:00:00.0000000', '0001-01-01 00:00:00.0000000');
+INSERT INTO Atracciones(Nombre, Imagen, Imagen2, Imagen3, Descripcion, ParqueId, Created, Updated)
+VALUES ('La Aripuca', 'aripuca.jpg', null, null, 'Parque ecológico con árboles rescatados.', 9, '0001-01-01 00:00:00.0000000', '0001-01-01 00:00:00.0000000');
